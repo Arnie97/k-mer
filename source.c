381a94347d14ec15619acf2b9b96074b7a37aeb6
Arnie97 <arnie97@gmail.com>
2015-05-08 01:14:07 +0800

    Calculates and prints elapsed time.

--- source.c
+++ source.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+
 #define k 5
 
 typedef struct occurrence {
@@ -10,8 +12,24 @@ typedef struct occurrence {
 new_t memory[1000000 * k];
 new_t *types[1 << (2 * k)];
 
+void
+elapsed_time(void)
+{
+    static clock_t interval;
+    if (!interval) {
+        interval = clock();
+    } else {
+        interval = clock() - interval;
+        printf("Well done in %.3f seconds.\n", (double)interval / CLOCKS_PER_SEC);
+        interval = 0;
+    }
+}
+
 int
 main(int argc, char const *argv[])
 {
+    elapsed_time();
+    for (int i = 0; i < 123456789; i++);
+    elapsed_time();
     return 0;
 }
