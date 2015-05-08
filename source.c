53248626202edfdee54c48689639cd88e2c5a21c
Arnie97 <arnie97@gmail.com>
2015-05-08 13:31:57 +0800

    Reads DNA sequences from files.

--- source.c
+++ source.c
@@ -36,10 +36,57 @@ throw(int error_code, char *message)
 }
 
 int
+load_input(FILE *fp)
+{
+    int sequence = 0, position, k_mer_type;
+    for (char c = '\n'; c != EOF; c = fgetc(fp)) {
+        if (c == '\n') {
+            while ((c = fgetc(fp)) != EOF && c != '\n');
+            sequence++;
+            position = -5;
+            k_mer_type = 0;
+            puts("========");
+            continue;
+        }
+
+        k_mer_type >>= 2;
+        switch (c) {
+        case 'A': /* k_mer_type ||= 0 << (2 * k - 2); */ break;
+        case 'C': k_mer_type |= 1 << (2 * k - 2); break;
+        case 'G': k_mer_type |= 2 << (2 * k - 2); break;
+        case 'T': k_mer_type |= 3 << (2 * k - 2); break;
+        default:
+            throw(3, "Unexpected character.");
+        }
+
+        if (++position < 0) {
+            continue;
+        }
+        printf("%d\n", k_mer_type);
+    }
+    return 0;
+}
+
+int
 main(int argc, char const *argv[])
 {
-    elapsed_time();
-    for (int i = 0; i < 123456789; i++);
-    elapsed_time();
+    if (argc < 2) {
+        throw(1, "No input files.");
+    }
+
+    for (int i = 1; i < argc; i++) {
+        printf("Opening %s...\n", argv[i]);
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            throw(2, "Unable to open specified file.");
+        }
+
+        printf("Start loading %s...\n", argv[i]);
+        elapsed_time();
+        load_input(fp);
+        elapsed_time();
+
+        fclose(fp);
+    }
     return 0;
 }
